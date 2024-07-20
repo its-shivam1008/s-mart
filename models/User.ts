1701a988d75855b951141ef32879ccb8004b2f4f
its-shivam1008 <tanil5960@gmail.com>
2024-07-20 10:39:03 +0530

    USER model and page.tsx modified

--- models/User.ts
+++ models/User.ts
@@ -11,18 +11,95 @@ export interface User extends Document{
     username:string;
     email:string;
     password:string;
-    imageeLogo:string;
+    imageLogo:string;
     address:{
         address:string;
         street:string;
         pincode:number;
         state:string;
-        country:string;
+        city:string;
     }
-    constact:string;
+    contact:string;
     isAdmin:boolean;
     cart:CartObj[];
     createdAt:Date;
     updatedAt:Date;
 }
 
+const UserSchema:Schema<User> = new Schema({
+    name:{
+        type:String,
+        required:[true, "name is required"]
+    },
+    username:{
+        type:String,
+        required:[true, "username is required"],
+        unique:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:[true, "email is required"],
+        unique:true,
+        match:[/.+\@.+\..+/, "Please provide a valid email address"]
+    },
+    password:{
+        type:String,
+        required:[true, "password is required"]
+    },
+    imageLogo:{
+        type:String,
+        required:[true, "icon is required"]
+    },
+    address:{
+        address:{
+            type:String
+        },
+        street:{
+            type:String
+        },
+        pincode:{
+            type:Number
+        },
+        state:{
+            type:String
+        },
+        city:{
+            type:String
+        },
+    },
+    contact:{
+        type:String,
+    },
+    isAdmin:{
+        type:Boolean
+    },
+    cart:[
+        {
+            productId:{
+                type:Schema.Types.ObjectId,
+                ref:'Product',
+                required:true
+            },
+            price:{
+                type:Number,
+                required:true
+            },
+            quantity:{
+                type:Number,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now
+    },
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+export default UserModel;
