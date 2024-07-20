bdbd0f9d3f75798d1dd17919f44b4326b31967a0
its-shivam1008 <tanil5960@gmail.com>
2024-07-20 14:23:29 +0530

    zod integration trial

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,10 +1,16 @@
 import dbConnect from "@/Db/Db";
 import UserModel from "@/models/User";
 import { NextResponse } from "next/server";
+import { signUpSchema } from "@/schemas/signUpSchema";
 
 export async function POST(request:Request){
     await dbConnect();
     const res = await request.json();
+    signUpSchema.parse({
+        email:res.email,
+        username:res.username,
+        password:res.password
+    })
     const newUser = new UserModel(res);
     const response = await newUser.save();
     return NextResponse.json({response, status:200})
