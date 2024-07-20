9f7e87bd2fffadc40196104d794d72e1f4b003ea
its-shivam1008 <tanil5960@gmail.com>
2024-07-20 11:20:59 +0530

    db connection and signupschema

---
+++ Db/Db.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?:number;
+}
+
+const connection:ConnectionObject = {}
+
+async function dbConnect():Promise<void>{
+    if(connection.isConnected){
+        console.log("Already connected to database");
+        return;
+    }else{
+        try{
+            if (process.env.MONGO_URI) {
+                const db = await mongoose.connect(process.env.MONGO_URI);
+                connection.isConnected = db.connections[0].readyState;
+                console.log("DB connected successfully");
+                console.log("db")
+                console.log(db)
+                console.log("db.connections")
+                console.log(db.connections)
+                console.log("db.connections[0]")
+                console.log(db.connections[0])
+                console.log("db.connections[0].readyState");
+                console.log(db.connections[0].readyState);
+              } else {
+                throw new Error("MONGO_URI environment variable is not set")
+              }
+        }catch(error){
+            console.log("DB connection failed error", error);
+            process.exit(1);
+        }
+    }
+}
+
+export default dbConnect;
---
+++ schemas/signUpSchema.ts
@@ -0,0 +1,12 @@
+import {z} from "zod";
+export const usernameValidation = z
+    .string()
+    .min(2,"username at least 2 characters")
+    .max(20, "username not more than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "username not contain any special charcters")
+
+export const signUpSchema = z.object({
+    username:usernameValidation,
+    email:z.string().email({message:"Invalid email address"}),
+    password:z.string().min(6,{message:"Password must be atleast of 6 characters"})
+})
