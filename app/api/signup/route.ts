6bd14f4d5501e90bab46330d0b22d5ec725816d0
its-shivam1008 <tanil5960@gmail.com>
2024-07-21 21:01:43 +0530

    zod is integrated during signup

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -3,15 +3,23 @@ import UserModel from "@/models/User";
 import { NextResponse } from "next/server";
 import { signUpSchema } from "@/schemas/signUpSchema";
 
-export async function POST(request:Request){
-    await dbConnect();
-    const res = await request.json();
-    signUpSchema.parse({
-        email:res.email,
-        username:res.username,
-        password:res.password
-    })
-    const newUser = new UserModel(res);
-    const response = await newUser.save();
-    return NextResponse.json({response, status:200})
+export async function POST(req:Request, res:Response){
+    try{
+        await dbConnect();
+        const data = await req.json();
+        const payload = {
+            email:data.email,
+            username:data.username,
+            password:data.password
+        }
+        const validation = signUpSchema.safeParse(payload);
+        if(!validation.success){
+            return NextResponse.json({message:validation.error.issues[0].message},{status:400})
+        }
+        const newUser = new UserModel(res);
+        const response = await newUser.save();
+        return NextResponse.json({response},{status:200});
+    }catch(err){
+        return NextResponse.json({message: "Internal server error"},{ status:500});
+    }
 }
