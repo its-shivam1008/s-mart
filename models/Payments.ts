30d3f9edf7d847831d188d73e3b3ad7c2d5ca5f8
its-shivam1008 <tanil5960@gmail.com>
2024-07-23 12:06:30 +0530

    payment model is created

---
+++ models/Payments.ts
@@ -0,0 +1,63 @@
+import mongoose, {Schema, Document, Types} from "mongoose";
+
+interface Payment extends Document{
+    toStore:{
+        storeName:string;
+        storeId:Types.ObjectId;
+    },
+    product:{
+        productName:string;
+        productId:Types.ObjectId;
+    },
+    fromUser:{
+        username:string;
+        userId:Types.ObjectId;
+    },
+    orderId:String;
+    isPaymentVerified:boolean;
+    dateofPayment:Date;
+    amount:Number;
+    paymentMethod:string;
+    transactionId:string;
+}
+
+const PaymentSchema:Schema<Payment> = new Schema({
+    toStore:{
+        storeName:String,
+        storeId:Schema.Types.ObjectId
+    },
+    product:{
+        productName:String,
+        productId:Schema.Types.ObjectId
+    },
+    fromUser:{
+        username:String,
+        userId:Schema.Types.ObjectId
+    },
+    orderId:{
+        type:String,
+    },
+    isPaymentVerified:{
+        type:Boolean,
+        default:false
+    },
+    dateofPayment:{
+        type:Date,
+        default:Date.now
+    },
+    amount:{
+        type:Number,
+        required:[true, "Amount is required (in paise)"]
+    },
+    paymentMethod:{
+        type:String,
+        enum:["razorpay", "stripe", "paytm", "bharatpe", "phonepe"]
+    },
+    transactionId:{
+        type:String
+    }
+})
+
+const PaymentModel = (mongoose.models.Payment as mongoose.Model<Payment>) || mongoose.model<Payment>("Payments", PaymentSchema);
+
+export default PaymentModel;
