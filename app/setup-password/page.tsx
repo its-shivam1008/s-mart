f051846d51401703fdbdc6d3100fba2dfeaffa38
its-shivam1008 <tanil5960@gmail.com>
2024-07-24 14:23:52 +0530

    saving the password through setup-password

--- app/setup-password/page.tsx
+++ app/setup-password/page.tsx
@@ -1,21 +1,35 @@
 "use client"
 import React, { InputHTMLAttributes } from 'react'
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from 'next/navigation';
 import { useState } from 'react'
 
 const page = () => {
     const [textValue, setTextValue] = useState("");
+    const { data: session, status } = useSession()
+    const router = useRouter();
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void  =>{
         setTextValue(e.target.value)
     }
-    const submitThePass = ()=>{
+    const submitThePass =async(e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
         // todo:
         // fetch to password saving route  to save the password
-
+        const res = await fetch('http://localhost:3000/app/api/setup-password',{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(session),
+        })
+        const data = await res.json();
+        console.log(data);
         // redirecting the user to verify itself
+        router.push('/verify');
     }
   return (
     <div>
-        <form action={submitThePass}>
+        <form onSubmit={submitThePass}>
             <label htmlFor="password">Enter password:</label>
             <input type="password" name="password" id="password" value={textValue} onChange={handleChange}/>
             <button type="submit" className='bg-blue-500 text-white rounded-full px-3 py-2 cursor-pointer'>Save password</button>
---
+++ app/verify/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      verify yourself
+    </div>
+  )
+}
+
+export default page
