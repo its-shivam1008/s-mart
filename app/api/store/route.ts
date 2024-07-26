a71e57cfcc7cfd93be9a73077728cb0626479e3f
its-shivam1008 <tanil5960@gmail.com>
2024-07-26 23:14:19 +0530

    store information update route added

--- app/api/store/route.ts
+++ app/api/store/route.ts
@@ -29,3 +29,28 @@ export async function POST(req:Request){
         return NextResponse.json({message: "Internal server error", success:false},{ status:500});
     }
 }
+
+export async function PUT(req:Request){
+    await dbConnect();
+    try{
+        const data = await req.json();
+        const userByEmail = await UserModel.findOne({email:data.session.user.email});
+        // checking the user is present in the db or not, if yes is he signed up as a store owner or not 
+        if(!userByEmail){
+            return NextResponse.json({message: "user not saved try to sign up again", success:false},{ status:404});
+        }else if(userByEmail?.role !== 'StoreOwner'){
+            return NextResponse.json({message:"please SignUp as a Store Owner to continue.", success:false}, {status:404});
+        }
+        //validating the data using zod
+        // todo:
+
+        // updating the store information 
+        await StoreModel.findOneAndUpdate({userId:userByEmail._id}, data.payload);
+
+        // send a welcome email
+        // todo :
+        return NextResponse.json({message:"Updated the store data", success:true},{status:201})
+    }catch(err){
+        return NextResponse.json({message: "Internal server error", success:false},{ status:500});
+    }
+}
