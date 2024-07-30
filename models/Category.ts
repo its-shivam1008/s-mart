ece4350fc31fd70dd187e110eaedaeabc4a4d518
its-shivam1008 <tanil5960@gmail.com>
2024-07-30 22:52:19 +0530

    category model implemented with sub and parent categories

--- models/Category.ts
+++ models/Category.ts
@@ -1,21 +1,41 @@
 import mongoose, {Schema, Types, Document} from "mongoose";
 
-interface Category extends Document {
+interface SubCategoryArray{
     name:string;
     description:string;
+    properties:[];
+    subCategoryId:Types.ObjectId;
+}
+
+interface ParentCategory extends Document {
+    name:string;
+    description:string;
+    subCategory:SubCategoryArray[];
     createdAt:Date;
     updatedAt:Date;
 }
 
-const CategorySchema:Schema<Category>=new Schema({
+const ParentCategorySchema:Schema<ParentCategory>=new Schema({
     name:{
         type:String,
+        unique:true,
         reqired:[true, "Category name is required"]
     },
     description:{
         type:String,
         reqired:[true, "description is required"]
     }, 
+    subCategory:[
+        {
+            name:String,
+            description:String,
+            properties:[],
+            subCategoryId:{
+                type:Schema.Types.ObjectId,
+                ref:'SubCategory'
+            }
+        }
+    ],
     createdAt:{
         type:Date,
         default:Date.now
@@ -26,6 +46,42 @@ const CategorySchema:Schema<Category>=new Schema({
     },
 })
 
-const CategoryModel = (mongoose.models.Category as mongoose.Model<Category>) || mongoose.model<Category>("Category", CategorySchema);
+const ParentCategoryModel = (mongoose.models.ParentCategory as mongoose.Model<ParentCategory>) || mongoose.model<ParentCategory>("ParentCategory", ParentCategorySchema);
 
-export default CategoryModel;
+interface SubCategory extends Document{
+    name:string;
+    description:string;
+    properties:[];
+    parentCategoryId:Types.ObjectId;
+    createdAt:Date;
+    updatedAt:Date;
+}
+
+const SubCategorySchema:Schema<SubCategory> = new Schema({
+    name:{
+        type:String,
+        unique:true,
+        reqired:[true, "SubCategory name is required"]
+    },
+    description:{
+        type:String,
+        reqired:[true, "SubCategory description is required"]
+    }, 
+    properties:[],
+    parentCategoryId:{
+        type:Schema.Types.ObjectId,
+        ref:'ParentCategory'
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now
+    },
+})
+
+const SubCategoryModel = (mongoose.models.SubCategory as mongoose.Model<SubCategory>) || mongoose.model<SubCategory>('SubCategory', SubCategorySchema)
+
+export default {SubCategoryModel, ParentCategoryModel};
