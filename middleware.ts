0b6052e165aa8cf11b528c6d2578fac5a5f79736
its-shivam1008 <tanil5960@gmail.com>
2024-08-01 13:56:01 +0530

    grouped signIn, signUp, verify, setup-password pages into auth

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
+export {default} from 'next-auth/middleware'
 import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
@@ -13,7 +14,10 @@ export async function middleware(request: NextRequest) {
     )){
         return NextResponse.redirect(new URL('/', request.url))
     }
-  return NextResponse.redirect(new URL('/home', request.url))
+    if(!token && url.pathname.startsWith('/')){
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+    return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more
