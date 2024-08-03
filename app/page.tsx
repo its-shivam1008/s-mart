e8ec238f92fa7aed2066d87c4d4db9c3ec944163
its-shivam1008 <tanil5960@gmail.com>
2024-08-04 00:04:22 +0530

    fixed error of middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import Image from "next/image";
-
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Home() {
+
+  const { data: session, status } = useSession()
   return (
     <div>
       hello this side ecommerce app
+
+      <button type='button' onClick={()=>{signOut()}}>Sign out</button>
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -2,24 +2,24 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 export {default} from 'next-auth/middleware'
 import type { NextRequest } from 'next/server'
-import UserModel from './models/User'
+// import UserModel from './models/User'
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     const token = await getToken({req:request})
     const url = request.nextUrl
-    if(token){
-      const user = await UserModel.findOne({email:token?.email})
-      if(user){
-        if(!user?.password){
-          return NextResponse.redirect(new URL('/setup-password', request.url))
-        }
-      }
-    }
+    // if(token){
+    //   console.log(token.email)
+    //   const user = await UserModel.findOne({email:token?.email})
+    //   if(user){
+    //     if(!user?.password){
+    //       return NextResponse.redirect(new URL('/setup-password', request.url))
+    //     }
+    //   }
+    // }
     if(token &&  (
         url.pathname.startsWith('/login')||
-        url.pathname.startsWith('/sign-up')||
-        url.pathname.startsWith('/verify')
+        url.pathname.startsWith('/sign-up')
     )){
         return NextResponse.redirect(new URL('/', request.url))
     }
