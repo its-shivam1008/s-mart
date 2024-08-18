f6c727d64e39fc21e370c6abbeec8c4734da0321
its-shivam1008 <tanil5960@gmail.com>
2024-08-18 10:59:46 +0530

    form with accordian is created

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -27,7 +27,7 @@ import { Input } from "@/components/ui/input"
 import { Loader2, LoaderCircle } from 'lucide-react';
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { basicStoreInfo } from '@/schemas/basicStoreInfo'
+import { basicStoreInfo, businessAddress, paymentIntegration } from '@/schemas/StoreInfo'
 import * as z from 'zod'
 
 
@@ -45,20 +45,50 @@ const page = () => {
     }
   })
 
-  const onSubmit = () => {
+  const businessAddressForm = useForm<z.infer<typeof businessAddress>>({
+    resolver:zodResolver(businessAddress),
+    defaultValues:{
+      address:'',
+      street:'',
+      pincode:0,
+      city:'',
+      state:'',
+      country:'',
+    }
+  })
+
+  const paymentIntegrationForm = useForm<z.infer<typeof paymentIntegration>>({
+    resolver:zodResolver(paymentIntegration),
+    defaultValues:{
+      id:'',
+      secret:''
+    }
+  })
+
+  const onSubmitBasicInfo = () => {
+
+  }
+  const onSubmitBusinessAddress = () => {
+
+  }
+  const onSubmitPaymentIntergration = () => {
 
   }
   return (
-    <div className='w-full h-screen bg-blue-400 flex justify-center items-center'>
-      <div className="w-[50%] h-[50%] bg-orange-400">
+    <div className='w-full min-h-screen h-auto bg-[#f2f2f2] flex justify-center items-center'>
+      <div className="w-[80%] h-[50%]  py-28 my-auto mx-auto">
+        <div className=' w-fit mx-auto space-y-8 px-6 py-3'>
+          <h1 className='text-[rebeccapurple] font-extrabold text-center text-4xl'>Getting started with your store</h1>
+          <div className='text-gray-400 text-center text-normal'>Please fill these sections to publish your first product</div>
+        </div>
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Basic Info</AccordionTrigger>
+            <AccordionTrigger><div className='px-2 py-4 rounded-xl text-[#f2f2f2] mx-auto w-full bg-[rebeccapurple]/50 '>Basic Information</div></AccordionTrigger>
             <AccordionContent>
               <div className='flex flex-col justify-center items-center h-fit py-8 px-2 bg-gray-100'>
                 <div className='w-full max-w-md px-8 py-3 space-y-4 bg-white rounded-lg shadow-md '>
                   <Form {...basicInfoForm}>
-                    <form onSubmit={basicInfoForm.handleSubmit(onSubmit)} className='space-y-4'>
+                    <form onSubmit={basicInfoForm.handleSubmit(onSubmitBasicInfo)} className='space-y-4'>
                       <FormField
                         control={basicInfoForm.control}
                         name="owner_name"
@@ -158,17 +188,157 @@ const page = () => {
         </Accordion>
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger><div className='px-2 py-4 rounded-xl text-[#f2f2f2] mx-auto w-full bg-[rebeccapurple]/50 '>Business Address</div></AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
+            <div className='flex flex-col justify-center items-center h-fit py-8 px-2 bg-gray-100'>
+                <div className='w-full max-w-md px-8 py-3 space-y-4 bg-white rounded-lg shadow-md '>
+                  <Form {...businessAddressForm}>
+                    <form onSubmit={businessAddressForm.handleSubmit(onSubmitBusinessAddress)} className='space-y-4'>
+                      <FormField
+                        control={businessAddressForm.control}
+                        name="address"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Address</FormLabel>
+                            <FormControl>
+                              <Input 
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={businessAddressForm.control}
+                        name="street"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Street</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Avenue park street"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={businessAddressForm.control}
+                        name="pincode"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Pincode</FormLabel>
+                            <FormControl>
+                              <Input type='number' placeholder="308001"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={businessAddressForm.control}
+                        name="city"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>City</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Jaipur"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={businessAddressForm.control}
+                        name="state"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>State</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Rajasthan"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={businessAddressForm.control}
+                        name="country"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Country</FormLabel>
+                            <FormControl>
+                              <Input placeholder="India"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <Button type='submit'>
+                        Save !
+                    </Button>
+                    </form>
+                  </Form>
+                </div>
+              </div>
             </AccordionContent>
           </AccordionItem>
         </Accordion>
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger><div className='px-2 py-4 rounded-xl text-[#f2f2f2] mx-auto w-full bg-[rebeccapurple]/50 '>Payment Integration</div></AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
+            <div className='flex flex-col justify-center items-center h-fit py-8 px-2 bg-gray-100'>
+                <div className='w-full max-w-md px-8 py-3 space-y-4 bg-white rounded-lg shadow-md '>
+                  <Form {...paymentIntegrationForm}>
+                    <form onSubmit={paymentIntegrationForm.handleSubmit(onSubmitPaymentIntergration)} className='space-y-4'>
+                      <FormField
+                        control={paymentIntegrationForm.control}
+                        name="id"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Razorpay Id</FormLabel>
+                            <FormControl>
+                              <Input 
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={paymentIntegrationForm.control}
+                        name="secret"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Razorpay secret</FormLabel>
+                            <FormControl>
+                              <Input 
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <Button type='submit'>
+                        Save !
+                    </Button>
+                    </form>
+                  </Form>
+                </div>
+              </div>
             </AccordionContent>
           </AccordionItem>
         </Accordion>
---
+++ schemas/StoreInfo.ts
@@ -0,0 +1,24 @@
+import {z} from 'zod';
+
+
+export const basicStoreInfo = z.object({
+    owner_name:z.string({required_error:'Owner name is required'}),
+    contact:z.string({required_error:'Contact is required'}),
+    businessName:z.string({required_error:'Business name is required'}),
+    storeName:z.string({required_error:'Store name is required'}),
+    category:z.string({required_error:'Category is required'})
+})
+
+export const businessAddress  = z.object({
+    address:z.string(),
+    street:z.string(),
+    pincode:z.number(),
+    state:z.string(),
+    city:z.string(),
+    country:z.string(),
+})
+
+export const paymentIntegration = z.object({
+    id:z.string(),
+    secret:z.string()
+})
