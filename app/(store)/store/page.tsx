373f99883584053992abf113d5f4c1ce0c16f0a9
its-shivam1008 <tanil5960@gmail.com>
2024-08-19 15:50:41 +0530

    orders page ui

--- app/(store)/store/page.tsx
+++ app/(store)/store/page.tsx
@@ -11,9 +11,41 @@ const page = () => {
   const router = useRouter();
 
   return (
-    <div className='my-20'>
-      
-      Analytics
+    <div className='min-h-screen bg-[#f2f2f2]'>
+      <div className="orders py-5">
+        <h1 className='p-5  text-purple-500 font-extrabold'>Orders</h1>
+        <div className='cards flex md:flex-row flex-col md:justify-evenly mx-auto w-fit md:w-auto space-y-8 md:space-y-0'>
+          <div className="card1 w-[200px] h-[150px] rounded-[12px] shadow-xl bg-gradient-to-br from-rose-200 to-teal-100 flex flex-col justify-evenly items-center text-[rebeccapurple]">
+            <h1 className="title text-xl font-bold">Today</h1>
+            <div className="number text-5xl font-extrabold">$$</div>
+          </div>
+          <div className="card2 w-[200px] h-[150px] rounded-[12px] shadow-xl bg-gradient-to-br from-rose-200 to-teal-100 flex flex-col justify-evenly items-center text-[rebeccapurple]">
+            <h1 className="title text-xl font-bold">This week</h1>
+            <div className="number text-5xl font-extrabold">$$</div>
+          </div>
+          <div className="card3 w-[200px] h-[150px] rounded-[12px] shadow-xl bg-gradient-to-br from-rose-200 to-teal-100 flex flex-col justify-evenly items-center text-[rebeccapurple]">
+            <h1 className="title text-xl font-bold">This month</h1>
+            <div className="number text-5xl font-extrabold">$$</div>
+          </div>
+        </div>
+      </div>
+      <div className="revenue py-5">
+        <h1 className='p-5 text-purple-500 font-extrabold'>Revenue</h1>
+        <div className='cards flex md:flex-row flex-col md:justify-evenly mx-auto w-fit md:w-auto space-y-8 md:space-y-0'>
+          <div className="card1 w-[200px] h-[150px] rounded-[12px] shadow-xl bg-gradient-to-br from-teal-200 to-rose-200 flex flex-col justify-evenly items-center text-[rebeccapurple]">
+            <h1 className="title text-xl font-bold">Today</h1>
+            <div className="number text-5xl font-extrabold">$$</div>
+          </div>
+          <div className="card2 w-[200px] h-[150px] rounded-[12px] shadow-xl bg-gradient-to-br from-teal-200 to-rose-200 flex flex-col justify-evenly items-center text-[rebeccapurple]">
+            <h1 className="title text-xl font-bold">This week</h1>
+            <div className="number text-5xl font-extrabold">$$</div>
+          </div>
+          <div className="card3 w-[200px] h-[150px] rounded-[12px] shadow-xl bg-gradient-to-br from-teal-200 to-rose-200 flex flex-col justify-evenly items-center text-[rebeccapurple]">
+            <h1 className="title text-xl font-bold">This month</h1>
+            <div className="number text-5xl font-extrabold">$$</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { ChartSpline, CirclePlus, Shirt, ShoppingBasket, User } from 'lucide-react';
+import { ChartSpline, CirclePlus, House, LogOut, Shirt, ShoppingBasket, User } from 'lucide-react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation'
@@ -17,15 +17,15 @@ const page = ({children}:{children:React.ReactNode}) => {
     <div className='grid grid-cols-[1fr,3fr] min-h-screen'>
       
       <div className=' p-5  bg-[#f2f2f2] space-y-20'>
-       <div>Home</div>
-       <div className="space-y-5">
-        <Link href={'/store'} className={`${currentPath== '/store' ? 'bg-gradient-to-r from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><ChartSpline className={`${currentPath== '/store' ? 'text-purple-400':'text-purple-300'} size-8`} />Analytics</Link>
-        <Link href={'/store/allProducts'} className={`${currentPath== '/store/allProducts' ? 'bg-gradient-to-r from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><Shirt className={`${currentPath== '/store/allProducts' ? 'text-purple-400':'text-purple-300'} size-8`} />Your Products</Link>
-        <Link href={'/store/addProduct'} className={`${currentPath=='/store/addProduct' ? 'bg-gradient-to-r from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><CirclePlus className={`${currentPath== '/store/addProduct' ? 'text-purple-400':'text-purple-300'} size-8`} />Add Product</Link>
-        <Link href={'/store/orders'} className={`${currentPath=='/store/orders' ? 'bg-gradient-to-r from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><ShoppingBasket className={`${currentPath== '/store/orders' ? 'text-purple-400':'text-purple-300'} size-8`} />Orders</Link>
-        <Link href={'/store/profile'} className={`${currentPath=='/store/profile' ? 'bg-gradient-to-r from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><User className={`${currentPath== '/store/profile' ? 'text-purple-400':'text-purple-300'} size-8`} />Profile</Link>
+       <Link href={'/'}><div className='text-purple-500 flex gap-2 items-center'><House className='text-purple-500 size-5' />Home</div></Link>
+       <div className="space-y-8">
+        <Link href={'/store'} className={`${currentPath== '/store' ? 'bg-gradient-to-r shadow-2xl from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><ChartSpline className={`${currentPath== '/store' ? 'text-purple-400':'text-purple-300'} size-8`} />Analytics</Link>
+        <Link href={'/store/allProducts'} className={`${currentPath== '/store/allProducts' ? 'bg-gradient-to-r shadow-2xl from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><Shirt className={`${currentPath== '/store/allProducts' ? 'text-purple-400':'text-purple-300'} size-8`} />Your Products</Link>
+        <Link href={'/store/addProduct'} className={`${currentPath=='/store/addProduct' ? 'bg-gradient-to-r shadow-2xl from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><CirclePlus className={`${currentPath== '/store/addProduct' ? 'text-purple-400':'text-purple-300'} size-8`} />Add Product</Link>
+        <Link href={'/store/orders'} className={`${currentPath=='/store/orders' ? 'bg-gradient-to-r shadow-2xl from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><ShoppingBasket className={`${currentPath== '/store/orders' ? 'text-purple-400':'text-purple-300'} size-8`} />Orders</Link>
+        <Link href={'/store/profile'} className={`${currentPath=='/store/profile' ? 'bg-gradient-to-r shadow-2xl from-rose-100 to-teal-100 text-purple-500 rounded-full p-3':'text-[rebeccapurple]'} flex gap-4 items-center font-bold text-xl`}><User className={`${currentPath== '/store/profile' ? 'text-purple-400':'text-purple-300'} size-8`} />Profile</Link>
        </div>
-       <div>Logout</div>
+       <div className='text-purple-500 flex gap-2 items-center'><LogOut className='text-purple-500 size-5' />Logout</div>
       </div>
       <div className=''>{children}</div>
     </div>
