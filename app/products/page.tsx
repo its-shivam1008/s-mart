e534df564add189a9f4649bd520930dad37c8dd9
its-shivam1008 <tanil5960@gmail.com>
2024-08-28 12:23:08 +0530

    image automatic slider

---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+import SlideShow from '@/components/SlideShow'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda impedit, voluptates iste repellat odit nemo. Perspiciatis officiis omnis quia accusamus cupiditate minima accusantium illum officia non. Perferendis porro nesciunt, ipsam architecto veniam culpa ab accusantium ad, error rerum ipsa unde aut natus voluptatibus ducimus, necessitatibus voluptas odit. Neque laudantium repellendus quaerat modi mollitia corporis numquam, velit fugiat magnam molestiae consequatur repellat veritatis ab consequuntur recusandae, unde fugit, quibusdam illum sit tempore quidem iste ex ea excepturi. Laborum iusto eos at quibusdam ipsa, delectus incidunt, tempora beatae cumque nulla maiores esse dolores odit necessitatibus dolore qui porro laboriosam ut illum! Amet.
+        <SlideShow/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/SlideShow.tsx
@@ -0,0 +1,48 @@
+'use client'
+import Image from 'next/image'
+import React, { useEffect, useRef, useState } from 'react'
+
+const SlideShow = () => {
+    const images = ['/categoryImages/categoryHome.jpg', '/categoryImages/categoryFashion.jpeg', '/categoryImages/categorySports.jpg']
+    const [index, setIndex] = useState(0);
+    const timeoutRef = useRef<any>();
+
+    function resetTimeout() {
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current);
+        }
+      }
+
+    useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+          () =>
+            setIndex((prevIndex) =>
+              prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            ),
+          2500
+        );
+        return () => {resetTimeout();};
+      }, [index]);
+
+  return (
+    <div className="slideshow overflow-hidden max-w-[500px] mx-auto my-0">
+      <div className={`slideshowSlider whitespace-nowrap  [transition:ease_1000ms]`} style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+        {images.map((img, index) => (
+            <div className="slide  h-[400px] w-full inline-block rounded-[40px]" key={index}>
+                <Image src={img} alt='noImg found' width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit:'cover'}}/>
+            </div>
+        ))}
+      </div>
+      <div className="slideshowDots text-center">
+        {images.map((_, idx) => (
+          <div  key={idx} onClick={() => {
+            setIndex(idx);
+          }} className={`slideshowDot ${index === idx ? "bg-[#6a0dad]" : ""} inline-block h-5 w-5 cursor-pointer bg-[#c4c4c4] mt-[15px] mb-0 mx-[7px] rounded-[50%]`}></div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SlideShow
