6367c9184eaf1c29768770b5913f91053b392b4b
its-shivam1008 <tanil5960@gmail.com>
2024-08-29 13:06:38 +0530

    Product component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,9 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <SlideShow/>
+        <SlideShow/>        
+        
+        
     </div>
   )
 }
--- components/ProductComponent.tsx
+++ components/ProductComponent.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
+import ProductCards from './productCards'
+import { Product } from '@/models/Product'
 
-const ProductComponent = () => {
+interface ProductData {
+    productData:Product[]
+}
+
+const ProductComponent:FunctionComponent<ProductData> = ({productData}) => {
   return (
-    <div>ProductComponent</div>
+    <div className='container'>
+        <div className="md:grid md:grid-cols-4 flex flex-col gap-5 justify-center">
+            {productData.map((product, index) => {
+                return <div key={index}>
+                    <ProductCards cardInfo={product}/>
+                </div>
+            })}
+        </div>
+    </div>
   )
 }
 
