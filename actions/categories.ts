8060e361bd963889ed16d126e3d97728fd636da1
its-shivam1008 <tanil5960@gmail.com>
2024-09-10 13:30:16 +0530

    Fetching 4 products from the db in category

--- actions/categories.ts
+++ actions/categories.ts
@@ -1,6 +1,7 @@
 'use server'
 import dbConnect from "@/Db/Db"
 import ParentCategoryModel from "@/models/ParentCategory"
+import ProductModel from "@/models/Product"
 
 export const fetchCategories = async () => {
     await dbConnect()
@@ -24,14 +25,12 @@ export const fetchCategories = async () => {
 export const fetchCategoryProduct = async (categoryName:string) => {
     await dbConnect()
     try{
-        const categories = await ParentCategoryModel.find()
-        const ArrayCategoryNames = []
-        for(let a of categories){
-            ArrayCategoryNames.push(a.name);
-        }
-        if(ArrayCategoryNames.length > 0){
-            const stringArray = JSON.stringify(ArrayCategoryNames)
-            return { message: 'Some error occured', categoriesArray: stringArray, success: false }  
+        const products = await ProductModel.find({'category.parentCategory.name':categoryName}).limit(4)
+        console.log(products)
+        if(products.length > 0){
+            const stringArray = JSON.stringify(products)
+            console.log('producst string', stringArray)
+            return { message: 'Some error occured', categoryProducts: stringArray, success: false }  
         }
     } catch (err) {
         return { message: 'Some error occured', error: err, success: false }
--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,17 +1,21 @@
 'use client'
-import { fetchCategories } from '@/actions/categories'
+import { fetchCategories, fetchCategoryProduct } from '@/actions/categories'
 import CardSkeletonLoading from '@/components/CardSkeletonLoading'
 import Loading from '@/components/Loading'
+import ProductCards from '@/components/ProductCards'
 import SlideShow from '@/components/SlideShow'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 
 const page = () => {
-    const images = ['/categoryImages/imageSlider1.jpg', '/categoryImages/imageSlider2.jpg', '/categoryImages/imageSlider3.jpg',  '/categoryImages/imageSlider4.jpg', '/categoryImages/imageSlider5.jpg', '/categoryImages/imageSlider6.jpg']
+    const images = ['/categoryImages/imageSlider1.jpg', '/categoryImages/imageSlider2.jpg', '/categoryImages/imageSlider3.jpg', '/categoryImages/imageSlider4.jpg', '/categoryImages/imageSlider5.jpg', '/categoryImages/imageSlider6.jpg']
 
     const [categoryNameArrayObj, setCategoryNameArrayObj] = useState<any>([])
 
+    const [categoryProducts, setCategoryProducts] = useState<any>({})
+
     const [isCategoriesLoading, setIsCategoriesLoading] = useState(false);
+    const [isProductLoading, setIsProductLoading] = useState(false)
 
 
 
@@ -19,46 +23,68 @@ const page = () => {
         (async () => {
             setIsCategoriesLoading(true)
             const categories = await fetchCategories();
-            if(categories && categories.success){
+            if (categories && categories.success) {
                 const categoryNameArrayObj = JSON.parse(categories.categoriesArray as string)
                 setCategoryNameArrayObj(categoryNameArrayObj)
             }
             setIsCategoriesLoading(false)
         })()
     }, [])
-    
 
-  return (
-    <div>
-        <SlideShow arrayOfImages={images} imageHeight='h-[80vh]'/>        
-        <div className='container mx-auto'>
-            {
-                isCategoriesLoading ? 
-                <div className='mx-auto'><Loading /></div> 
-                    : 
-                    categoryNameArrayObj.map((element:any, index:number) =>{
-                        return (<div key={index} className='flex flex-col gap-4 my-10 font-mono'>
-                            <Link href={`/categories/${element.id}`} className='text-2xl font-bold transition-colors duration-300 hover:text-purple-500'>{element.name}</Link>
-                            <div className='flex md:flex-row  flex-col justify-around items-center'>
-                                {
-                                    [...Array(4)].map((element, index) =>{ 
-                                        return (
-                                            <div key={index}>
-                                                <CardSkeletonLoading />
-                                            </div>
-                                        )
-                                    })
-                                }
-                                <Link href={`/categories/${element.id}`} className='w-48 h-60 flex justify-center items-center bg-gray-300 rounded-[12px] shadow-xl shadow-purple-500'>
-                                    <div className='text-gray-800 font-mono text-xl font-bold transition-colors duration-500 hover:text-purple-800 cursor-pointer'>See all</div>
-                                </Link>
-                            </div>
-                        </div>)
-                    })
-            }
+    useEffect(() => {
+        if (categoryNameArrayObj.length > 0) {
+            (async () => {
+                setIsProductLoading(true)
+                for (let a of categoryNameArrayObj) {
+                    const arrayOfProducts = await fetchCategoryProduct(a.name)
+                    if(arrayOfProducts && arrayOfProducts.success){
+                        const aId = a.id
+                        const productObj = {
+                            aId:JSON.parse(arrayOfProducts?.categoryProducts as string)
+                        }
+                        setCategoryProducts((prevState:any) => ({...prevState, ...productObj}))   
+                        console.log(categoryProducts)
+                    }
+                }
+                setIsProductLoading(false)
+            })()
+        }
+    }, [])
+
+
+    return (
+        <div>
+            {/* <SlideShow arrayOfImages={images} imageHeight='h-[80vh]' /> */}
+            <div className='container mx-auto'>
+                {
+                    isCategoriesLoading ?
+                        <div className='mx-auto'><Loading /></div>
+                        :
+                        categoryNameArrayObj.map((element: any, index: number) => {
+                            return (<div key={index} className='flex flex-col gap-4 my-10 font-mono'>
+                                <Link href={`/categories/${element.id}`} className='text-2xl font-bold transition-colors duration-300 hover:text-purple-500'>{element.name}</Link>
+                                <div className='flex md:flex-row  flex-col justify-around items-center'>
+                                    {
+                                        categoryProducts[element.id]?.map((elem:any, index:number) => {
+                                            return (
+                                                <div key={index}>
+                                                    {
+                                                        isProductLoading ? <CardSkeletonLoading/> : <ProductCards cardInfo={elem}/>
+                                                    }
+                                                </div>
+                                            )
+                                        })|| <p>No products found</p>
+                                    }
+                                    <Link href={`/categories/${element.id}`} className='w-48 h-60 flex justify-center items-center bg-gray-300 rounded-[12px] shadow-xl shadow-purple-500'>
+                                        <div className='text-gray-800 font-mono text-xl font-bold transition-colors duration-500 hover:text-purple-800 cursor-pointer'>See all</div>
+                                    </Link>
+                                </div>
+                            </div>)
+                        })
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default page
