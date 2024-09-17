4086a99e4cef28a9a86973f1d3876865856105b5
its-shivam1008 <tanil5960@gmail.com>
2024-09-17 12:42:41 +0530

    conditional smooth scrolling

--- components/SmoothScrolling.jsx
+++ components/SmoothScrolling.jsx
@@ -1,12 +1,22 @@
 "use client";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import { usePathname } from 'next/navigation'
 
 function SmoothScrolling({ children }) {
-  return (
-    <ReactLenis root options={{ lerp: 0.03, duration: 1.7, smoothTouch: true }}>
-      {children}
-    </ReactLenis>
+  const pathname = usePathname();
+  const pathsWithoutLenis = ['/ask-ai','/store/allProducts'];
+  const disableLenis = pathsWithoutLenis.includes(pathname);
+  if (disableLenis) {
+    // console.log('ye return hua hai')
+    return <>{children}</>;
+  }else{
+    // console.log('lenis lag gya hai ', pathname)
+    return (
+      <ReactLenis root options={{ lerp: 0.03, duration: 1.7, smoothTouch: true }}>
+        {children}
+      </ReactLenis>
   );
+  }
 }
 
 export default SmoothScrolling;
