c1bf2afa7553a1a478a9d799feea53610c232f7b
its-shivam1008 <tanil5960@gmail.com>
2024-09-20 12:00:10 +0530

    orders page for store owners

--- app/(store)/store/orders/page.tsx
+++ app/(store)/store/orders/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
 
     const fetchOrdersFromDB = async (userEmail: string) => {
         // console.log(session);
-        setIsLoading(true)
+        
         const userType = await checkUserType(userEmail)
         if (userType?.userRole !== 'StoreOwner') {
             router.push('/')
@@ -40,26 +40,29 @@ const page = () => {
             console.log(orders.data.orders, "orders")
             setOrdersArray(orders.data.orders)
         }
-        setIsLoading(false)
+        
     }
 
     useEffect(() => {
         if (session && !flag) {
-            console.log(session)
+            // console.log(session)
+            setIsLoading(true)
             fetchOrdersFromDB(session.user.email as string)
+            setIsLoading(false)
             setFlag(true)
         }
-    }, [session, flag])
+    }, [session, flag, ordersArray])
 
     const handleStatusChange = async (orderId:any, value:string) => {
         const payload = {
             session
         }
-        const response = await axios.put(`/api/store/order?orderId=${orderId}&status=${value}`,payload)
+        const response = await axios.put(`/api/store/orders?orderId=${orderId}&status=${value}`,payload)
         if(response.data.success){
             toast({
                 title:'Status updated',
             })
+            fetchOrdersFromDB(session?.user.email as string)
         }else{
             toast({
                 variant: "destructive",
@@ -73,8 +76,9 @@ const page = () => {
         const response = await axios.delete(`/api/store/orders?orderId=${orderId}`)
         if(response.data.success){
             toast({
-                title:'Status updated',
+                title:'Order deleted',
             })
+            fetchOrdersFromDB(session?.user.email as string)
         }else{
             toast({
                 variant: "destructive",
@@ -87,7 +91,7 @@ const page = () => {
     return (
         <div className='bg-[#f2f2f2] min-h-screen'>
             <h1 className="text-3xl ml-5 mb-5 pt-5">Orders</h1>
-            <div className="overflow-auto w-full">
+            {ordersArray.length === 0 ? <div className='text-center text-2xl font-bold'>No orders present</div> :<div className="overflow-auto w-full">
                 {isLoading ? <div className='p-8 mx-auto'><Loading /></div> : <table className="my-10 border-3 border-solid border-[#521e52] w-full h-full table-auto border-collapse border-spacing-1 text-center overflow-x-auto w-inherit">
                     <caption className="caption-top text-center my-3">Track your orders</caption>
                     <thead>
@@ -101,9 +105,10 @@ const page = () => {
                     <tbody>
                         {ordersArray.map((element: any, index: number) => (
                             <tr key={index}>
-                                <td className="border-3 border-[#521e52] bg-[#f5d6f2] text-black p-2 flex items-center gap-2">{element.status === 'Cancelled' ? <div onClick={() => handleDeleteOrder(element._id)}><Trash2 className='text-red-500 size-5'/></div>:''}{element.user.userEmail}</td>
+                                <td className={`border-3 border-[#521e52] bg-[#f5d6f2] text-black p-2 space-x-2`}>{element.user.userEmail}</td>
                                 <td className="border-3 border-[#521e52] bg-[#f5d6f2] text-black p-2">{element.shippingAddress.address}, {element.shippingAddress.street}, {element.shippingAddress.state}, {element.shippingAddress.city}, {element.shippingAddress.pincode}</td>
                                 <td className="border-3 border-[#521e52] bg-[#f5d6f2] text-black p-2">
+                                {element.status === 'Cancelled' ? <div className='cursor-pointer' onClick={() => handleDeleteOrder(element._id)}><Trash2 className='text-red-500 size-5 '/></div>:
                                     <Select value={element.status} onValueChange={(value:string) => handleStatusChange(element._id, value)}>
                                         <SelectTrigger className="w-fit">
                                             <SelectValue placeholder="Status" />
@@ -111,18 +116,18 @@ const page = () => {
                                         <SelectContent>
                                             <SelectItem value="Confirmed">Confirmed</SelectItem>
                                             <SelectItem value="Pending">Pending</SelectItem>
-                                            <SelectItem value="Cancelled">Cancelled</SelectItem>
                                             <SelectItem value="Shipped">Shipped</SelectItem>
                                             <SelectItem value="Delivered">Delivered</SelectItem>
                                         </SelectContent>
-                                    </Select>
+                                    </Select>   
+                                }
                                 </td>
                                 <td className="border-3 border-[#521e52] bg-[#f5d6f2] text-black p-2">{element._id}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>}
-            </div>
+            </div>}
         </div>
     )
 }
