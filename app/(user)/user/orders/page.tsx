d45ee9a2037f505697211899b9f56beff0b98de9
its-shivam1008 <tanil5960@gmail.com>
2024-09-24 23:10:14 +0530

    add orders page in users

---
+++ app/(user)/user/orders/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen bg-[#f2f2f2]'>
+        <div className="text-3xl font-bold p-5">Orders</div>
+    </div>
+  )
+}
+
+export default page
